f3e62badd4fbf11ad40b4248e3f3d62bc36a530d
Vahap Caliskan <a.vahapclskn@gmail.com>
2016-09-10 16:19:51 +0200

    Add files via upload

---
+++ ZahlenUmrechner/ZahlenUmrechner/ZahlenUmrechner.cpp
@@ -0,0 +1,23 @@
+// ZahlenUmrechner.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <stdlib.h>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout << "In welches Zahlensystem soll konvertiert werden?\n";
+	cout << "1. Hexadezimal\n2. Dezimal\n3. Oktal\n4. Binaer\n";
+	cout << "Zum Auswaehlen die gewuenschte Ziffer eingeben (1-4)\n";
+	cout << "Zum Beenden 'exit' eingeben\n";
+	string sMenu;
+	cin >> sMenu;
+}
+
